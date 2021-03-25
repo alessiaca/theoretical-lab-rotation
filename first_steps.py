ef0f98b62d2e4b2f2a4369a68be72ebbb490c17f
unknown <alessia.cavallo16@gmail.de>
2021-03-25 08:45:34 +0100

    Added leaky onset units

--- first_steps.py
+++ first_steps.py
@@ -5,23 +5,34 @@ from scipy.integrate import odeint
 # Next steps: Implement leaky onset units
 
 
+def pos_sat(x):
+    """"Returns 0 if x <= 0, else x"""
+    return np.max([x,0])
+
+
 class Unit:
 
     # Initialize the unit
-    def __init__(self, type, tau=300, theta=0, delta=1, u_0=10):
-        self.type = type  # Define the type of the unit: "leaky","leaky_onset", "excitatory" or "inhibitory"
+    def __init__(self, type, tau=300, theta=0, delta=1, u_0=10,tau_i=None,u_i_0=None):
+        self.type = type  # Define the type of the unit: "leaky","leaky onset", "excitatory" or "inhibitory"
+        self.input_units = []  # Initialize an empty list of input units
+        self.u = u_0
         self.tau = tau  # Define time constant of the unit
         self.theta = theta  # Define the steepness of the hyperbolic function for the unit activation
         self.delta = delta  # Define the activation threshold
-        self.u = u_0  # Initialize the potential of the unit
-        self.input_units = []  # Initialize an empty list of input units
+
+        if type == "leaky onset":
+            self.tau_i = tau_i
+            self.u_i = u_i_0
 
     # Compute the potential of the unit (dependent on the type)
     def func(self):
         if self.type == "leaky":
             return (-self.u + self.input()) / self.tau
-        if self.type == "leaky_onset":
-
+        elif self.type == "leaky onset":
+            u = (-self.u + pos_sat(self.input() - self.u_i)) / self.tau
+            u_i = (-self.u_i + self.input()) / self.tau_i
+            return u, u_i
 
     # Add a new input unit with a specific weight
     def add_input_unit(self, input_unit, input_weight):
@@ -29,7 +40,7 @@ class Unit:
 
     # Given the potential and the parameters, compute the activation of the unit
     def activation(self):
-        return np.max([np.tanh(self.delta * (self.u - self.theta)), 0])
+        return pos_sat(np.tanh(self.delta * (self.u - self.theta)))
 
     # Compute the input to the unit given the input units and connection weights
     def input(self):
@@ -54,7 +65,11 @@ def run_model(t_max, dt):
     for i_t in range(1, len(t)):
         for i_u,unit in enumerate(units.values()):
             activities[i_u,i_t] = unit.u
-            unit.u = unit.u + unit.func() * dt
+            if unit.type == "leaky":
+                unit.u = unit.u + unit.func() * dt
+            elif unit.type == "leaky onset":
+                unit.u = unit.u + unit.func()[0] * dt
+                unit.u_i = unit.u_i + unit.func()[1] * dt
 
     return activities
 
